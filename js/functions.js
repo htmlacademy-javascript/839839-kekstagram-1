50d3ffdace0d232c5bb13a1ecd71faf42a836d93
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-03-02 21:44:13 +0300

    добавлена функция getPalindromeCheck2

--- js/functions.js
+++ js/functions.js
@@ -12,3 +12,18 @@ getPalindromeCheck('топот'); // true
 getPalindromeCheck('ДовОд'); // true
 getPalindromeCheck('Кекс'); // false
 getPalindromeCheck('Лёша на полке клопа нашёл '); // true
+
+function getPalindromeCheck2(string) {
+  string = string.toLowerCase().replaceAll(' ', '');
+  for(let i = 1; i <= (string.length - 1); i++) {
+    if (string[i - 1] !== string.at(-i)) {
+      return false;
+    }
+  }
+  return true;
+}
+
+getPalindromeCheck2('топот'); // true
+getPalindromeCheck2('ДовОд'); // true
+getPalindromeCheck2('Кекс'); // false
+getPalindromeCheck2('Лёша на полке клопа нашёл '); // true
