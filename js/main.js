dbdcded95706b482978f6f2f6c3e383c376015e9
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-04-09 19:01:26 +0300

    перенос файлов в папку mocks и изменение путей

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import {getPhotos} from './data.js';
-import {PHOTO_COUNT} from './data-generation.js';
+import {getPhotos} from './mocks/data.js';
+import {PHOTO_COUNT} from './mocks/data-generation.js';
 
 getPhotos(PHOTO_COUNT);
