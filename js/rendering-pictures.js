6d2a496e16c5e32332984ff4d396e0ac31f6b762
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-05-05 15:45:44 +0300

    сохраняем клон шаблона

--- js/rendering-pictures.js
+++ js/rendering-pictures.js
@@ -3,4 +3,5 @@ import {PHOTO_COUNT} from './mocks/data-generation.js';
 
 const containerForPicture = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const clonePictureTemplate = pictureTemplate.cloneNode(true);
 
