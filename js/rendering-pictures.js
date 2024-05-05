2b3e68f6f54ce0fe8650f655644274265a472a4f
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-05-05 15:33:27 +0300

    подключение сгенерированных данных

--- js/rendering-pictures.js
+++ js/rendering-pictures.js
@@ -1,2 +1,6 @@
+import {getPhotos} from './mocks/data.js';
+import {PHOTO_COUNT} from './mocks/data-generation.js';
+
 const containerForPicture = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+
