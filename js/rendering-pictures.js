015ce087f1047ab8986516c842e8e310bf0e2a49
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-05-05 17:05:39 +0300

    отрисовка миниатюр

--- js/rendering-pictures.js
+++ js/rendering-pictures.js
@@ -3,5 +3,22 @@ import {PHOTO_COUNT} from './mocks/data-generation.js';
 
 const containerForPicture = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
-const clonePictureTemplate = pictureTemplate.cloneNode(true);
+
+/**
+ * Сборка миниатюры
+ * @param {Object} - данные миниатюры
+ */
+const getThumbnail = ({url, description, comments, likes}) => {
+  const clonePictureTemplate = pictureTemplate.cloneNode(true);
+  clonePictureTemplate.querySelector('.picture__img').src = url;
+  clonePictureTemplate.querySelector('.picture__img').alt = description;
+  clonePictureTemplate.querySelector('.picture__comments').textContent = comments.length;
+  clonePictureTemplate.querySelector('.picture__likes').textContent = likes;
+  return clonePictureTemplate;
+};
+
+getPhotos(PHOTO_COUNT).forEach((thumbnail) => {
+  containerForPicture.append(getThumbnail(thumbnail));
+});
+
 
