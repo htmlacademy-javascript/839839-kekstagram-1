115e56a75f72e63da1cc203efeb3be330c74951d
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-05-11 17:45:55 +0300

    вставка открываемого изображения

--- js/rendering-big-picture.js
+++ js/rendering-big-picture.js
@@ -1,6 +1,12 @@
 const picturesList = document.querySelector('.pictures');
 const bigPicture = document.querySelector('.big-picture');
 const buttonClose = bigPicture.querySelector('.big-picture__cancel');
+const picturePath = document.querySelector('.big-picture__img');
+
+const fillingPublicationData = (evt) => {
+  picturePath.querySelector('img').src = evt.target.src;
+  picturePath.querySelector('img').alt = evt.target.alt;
+};
 
 const onDocumentKeydown = (evt) => {
   if (evt.key === 'Escape') {
@@ -12,6 +18,7 @@ const onDocumentKeydown = (evt) => {
 const openPicture = (evt) => {
   if (evt.target.nodeName === 'IMG') {
     bigPicture.classList.remove('hidden');
+    fillingPublicationData(evt);
 
     document.addEventListener('keydown', onDocumentKeydown);
   }
@@ -27,4 +34,3 @@ const closePicture = (evt) => {
 picturesList.addEventListener('click', openPicture);
 
 buttonClose.addEventListener('click', closePicture);
-
