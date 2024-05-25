e184aedae207fe1af8dead322fa233c20955f3e6
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-05-26 02:15:12 +0300

    добавлено событие открытия формы редактирования изображения

--- js/form.js
+++ js/form.js
@@ -1,1 +1,18 @@
+const uploadFile = document.querySelector('#upload-file');
+const overlay = document.querySelector('.img-upload__overlay');
+const body = document.querySelector('body');
 
+const onDocumentKeydown = (evt) => {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    overlay.classList.add('hidden');
+  }
+};
+
+const onUploadFileChange = () => {
+  overlay.classList.remove('hidden');
+  body.classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
+};
+
+uploadFile.addEventListener('change', onUploadFileChange);
