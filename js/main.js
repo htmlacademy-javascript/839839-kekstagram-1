e1372e71527e1bb15671a10d4b110a4bb5f03bc3
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-05-27 16:25:08 +0300

    решение конфликта в main.js и подключение файла form.js

--- js/main.js
+++ js/main.js
@@ -2,8 +2,11 @@ import {getPhotos} from './mocks/data.js';
 import {PHOTO_COUNT} from './mocks/data-generation.js';
 import {renderingThumbnails} from './rendering-pictures.js';
 import {isEventBigPicture} from './big-picture.js';
+import {isEventUploadForm} from './form.js';
 
 const publicationsData = getPhotos(PHOTO_COUNT);
 renderingThumbnails(publicationsData);
 
 isEventBigPicture(publicationsData);
+
+isEventUploadForm();
