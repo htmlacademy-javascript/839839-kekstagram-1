3e2ead2cc261666615d0709c2fbacd28aa3e0a47
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-05-27 11:22:54 +0300

    валидация поля с хештегами

--- js/form.js
+++ js/form.js
@@ -1,15 +1,39 @@
+const TOTAL_HASHTAG = 5;
+
 const form = document.querySelector('.img-upload__form');
 const uploadFile = document.querySelector('#upload-file');
 const overlay = document.querySelector('.img-upload__overlay');
+const textHashtag = form.querySelector('.text__hashtags');
 const buttonCancel = document.querySelector('#upload-cancel');
+const description = document.querySelector('.text__description');
 const body = document.querySelector('body');
 
-const onDocumentKeydown = (evt) => {
-  if (evt.key === 'Escape') {
-    evt.preventDefault();
-    overlay.classList.add('hidden');
-  }
+const hashtagTest = /^#[a-zа-яё0-9]{1,19}$/i;
+
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextTag: 'div',
+  errorTextClass: 'form__error'
+});
+
+const hashtagCountValidation = (value) => {
+  const hashtags = value.trim().split(' ');
+  return hashtags.length <= TOTAL_HASHTAG;
 };
+
+const hashtagEntryFormValidation = (value) => {
+  const hashtags = value.trim().split(' ');
+  for (let i = 0; i < hashtags.length; i++) {
+    if (!hashtagTest.test(hashtags[i])) {
+      return false;
+    }
+  }
+  return true;
+};
+
+pristine.addValidator(textHashtag, hashtagEntryFormValidation, 'Не правильная форма записи хештега');
+pristine.addValidator(textHashtag, hashtagCountValidation, 'Не больше пяти хэш-тегов');
 
 const onUploadFileChange = () => {
   overlay.classList.remove('hidden');
@@ -24,9 +48,24 @@ const onButtonCancelClick = () => {
   document.removeEventListener('keydown', onDocumentKeydown);
 };
 
+const isInputFocus = () =>
+  document.activeElement === textHashtag ||
+  document.activeElement === description;
+
+function onDocumentKeydown(evt) {
+  if (evt.key === 'Escape' && !isInputFocus) {
+    evt.preventDefault();
+    onButtonCancelClick();
+  }
+}
+
 const isEventUploadForm = () => {
   uploadFile.addEventListener('change', onUploadFileChange);
   buttonCancel.addEventListener('click', onButtonCancelClick);
+  form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    pristine.validate();
+  });
 };
 
 isEventUploadForm();
