83b0a9123a988e04e89e23ad01dc69ea259f2cb3
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-06-27 03:00:56 +0300

    добавлены: файл api.js, реализовано получение данных с сервера и их отрисовка. обработка ошибки: "нет связи с сервером"

---
+++ js/api.js
@@ -0,0 +1,24 @@
+const BASE_URL = 'https://28.javascript.htmlacademy.pro/kekstagram';
+
+const Route = {
+  GET_DATA: '/data',
+  SEND_DATA: '/',
+};
+const ErrorText = {
+  GET_DATA: 'Не удалось загрузить данные. Попробуйте обновить страницу',
+  SEND_DATA: 'Не удалось отправить форму. Попробуйте ещё раз',
+};
+
+const getData = () => fetch(
+  `${BASE_URL}${Route.GET_DATA}`)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error();
+    }
+    return response.json();
+  })
+  .catch(() => {
+    throw new Error(ErrorText.GET_DATA);
+  });
+
+export {getData};
--- js/main.js
+++ js/main.js
@@ -1,15 +1,27 @@
-import {getPhotos} from './mocks/data.js';
-import {PHOTO_COUNT} from './mocks/data-generation.js';
+// import {getPhotos} from './mocks/data.js';
+// import {PHOTO_COUNT} from './mocks/data-generation.js';
 import {renderingThumbnails} from './rendering-pictures.js';
 import {addEventListenerThumbnail} from './big-picture.js';
 import {addEventUploadForm} from './form.js';
 import {addEventListenerButton} from'./scale.js';
 import {addEventListenerEffects} from './effect.js';
+import {showAlert} from './util.js';
+import {getData} from './api.js';
 
-const publicationsData = getPhotos(PHOTO_COUNT);
-renderingThumbnails(publicationsData);
+getData()
+  .then((publicationsData) => {
+    renderingThumbnails(publicationsData);
+    addEventListenerThumbnail(publicationsData);
+  })
+  .catch(
+    (err) => {
+      showAlert(err.message);
+    }
+  );
 
-addEventListenerThumbnail(publicationsData);
+// const publicationsData = getPhotos(PHOTO_COUNT);
+// renderingThumbnails(publicationsData);
+// addEventListenerThumbnail(publicationsData);
 
 addEventUploadForm();
 
