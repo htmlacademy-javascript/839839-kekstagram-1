6c896849cf6baf3bae7dc528fe3a69ffe71d83f4
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-07-09 01:46:54 +0300

    добавлены сортировки: обсуждаемые и случайные

--- js/filter.js
+++ js/filter.js
@@ -1,3 +1,4 @@
+const PICTURES_COUNT = 10;
 const Filter = {
   DEFAULT: 'filter-default',
   RANDOM: 'filter-random',
@@ -12,17 +13,37 @@ const showFilter = () => {
 
 showFilter();
 
-filterElement.addEventListener('click', (evt) => {
-  if (!evt.target.classList.contains('img-filters__button')) {
-    return;
-  }
-  const clickedButton = evt.target;
-  if (clickedButton === currentFilter) {
-    return;
-  }
-  filterElement.querySelector('.img-filters__button--active')
-    .classList.remove('img-filters__button--active');
-  clickedButton.classList.add('img-filters__button--active');
-  currentFilter = clickedButton.id;
+const sortByDiscussion = (pictureA, pictureB) =>
+  pictureB.comments.length - pictureA.comments.length;
 
-});
+const sortRamdom = () => Math.random() - 0.5;
+
+const filterPicture = (data) => {
+  switch (currentFilter) {
+    case Filter.DISCUSSED:
+      return console.log(data.slice().sort(sortByDiscussion));
+    case Filter.RANDOM:
+      return console.log(data.slice().sort(sortRamdom).slice(0, PICTURES_COUNT));
+    default:
+      return data;
+  }
+};
+
+const addEventListenerFilter = (dataPicture) => {
+  filterElement.addEventListener('click', (evt) => {
+    if (!evt.target.classList.contains('img-filters__button')) {
+      return;
+    }
+    const clickedButton = evt.target;
+    if (clickedButton === currentFilter) {
+      return;
+    }
+    filterElement.querySelector('.img-filters__button--active')
+      .classList.remove('img-filters__button--active');
+    clickedButton.classList.add('img-filters__button--active');
+    currentFilter = clickedButton.id;
+    filterPicture(dataPicture);
+  });
+};
+
+export {addEventListenerFilter};
