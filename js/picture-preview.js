de618799325e8bcd80e26adf22f7df6e140e37a6
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-07-09 22:37:58 +0300

    обернули обработчик событий в функцию addEventListenerFileChooser

--- js/picture-preview.js
+++ js/picture-preview.js
@@ -4,14 +4,18 @@ const fileChooser = document.querySelector('.img-upload__input');
 const photoPreview = document.querySelector('.img-upload__preview img');
 const effectsPreview = document.querySelectorAll('.effects__preview');
 
-fileChooser.addEventListener('change', () => {
-  const file = fileChooser.files[0];
-  const fileName = file.name.toLowerCase();
-  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
-  if (matches) {
-    photoPreview.src = URL.createObjectURL(file);
-    effectsPreview.forEach((preview) => {
-      preview.style.backgroundImage = `url('${photoPreview.src}')`;
-    });
-  }
-});
+const addEventListenerFileChooser = () => {
+  fileChooser.addEventListener('change', () => {
+    const file = fileChooser.files[0];
+    const fileName = file.name.toLowerCase();
+    const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+    if (matches) {
+      photoPreview.src = URL.createObjectURL(file);
+      effectsPreview.forEach((preview) => {
+        preview.style.backgroundImage = `url('${photoPreview.src}')`;
+      });
+    }
+  });
+};
+
+export {addEventListenerFileChooser};
