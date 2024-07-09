9a3f6fae73f8810fcc709565a1a9260766ba73af
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-07-09 22:25:56 +0300

    Загрузка изображения для предварительного просмотра

---
+++ js/picture-preview.js
@@ -0,0 +1,13 @@
+const FILE_TYPES = ['jpg', 'jpeg', 'png'];
+
+const fileChooser = document.querySelector('.img-upload__input');
+const photoPreview = document.querySelector('.img-upload__preview img');
+
+fileChooser.addEventListener('change', () => {
+  const file = fileChooser.files[0];
+  const fileName = file.name.toLowerCase();
+  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+  if (matches) {
+    photoPreview.src = URL.createObjectURL(file);
+  }
+});
