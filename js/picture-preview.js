8e629a666ec8179f0c73591dc47d4352fe7ef6ea
Mariya Koshelenko <k.mary.srg@gmail.com>
2024-07-09 22:27:54 +0300

    Загрузка изображения для фильтра фотографии предварительного просмотра

--- js/picture-preview.js
+++ js/picture-preview.js
@@ -2,6 +2,7 @@ const FILE_TYPES = ['jpg', 'jpeg', 'png'];
 
 const fileChooser = document.querySelector('.img-upload__input');
 const photoPreview = document.querySelector('.img-upload__preview img');
+const effectsPreview = document.querySelectorAll('.effects__preview');
 
 fileChooser.addEventListener('change', () => {
   const file = fileChooser.files[0];
@@ -9,5 +10,8 @@ fileChooser.addEventListener('change', () => {
   const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
   if (matches) {
     photoPreview.src = URL.createObjectURL(file);
+    effectsPreview.forEach((preview) => {
+      preview.style.backgroundImage = `url('${photoPreview.src}')`;
+    });
   }
 });
